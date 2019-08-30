80e971ab73ea66f51fffed510c697ca2379a3ec4
willanythayse <willanythayse@users.noreply.github.com>
2019-08-30 13:51:48 +0100

    Cuckoo Search with Michalewicz Function
    Otimização da Função Michalewicz.

--- R/fobj.R
+++ R/fobj.R
@@ -1,7 +1,9 @@
+source("R/michal.R")
+
 ## You can replace the following by your own functions
 # A d-dimensional objective function
 fobj <- function (u) {
   ## d-dimensional sphere function sum_j=1^d (u_j-1)^2. 
   #  with a minimum at (1,1, ...., 1); 
-  return(cs_aux(u))
+  return(michal(u))
 }
